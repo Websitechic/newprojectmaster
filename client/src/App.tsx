f02fad9d01d30553df9b958677013cedcf00d0ce
worktool <39032446-worktool@users.noreply.replit.com>
2025-01-30 09:04:57 +0000

    Agent query: Could you try registering again and see if it redirects you to the dashboard properly now?
    Add redirect to dashboard on root path.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/5e34c30e-b4e1-410a-9b6a-63e64c7461ae/c74ff4ef-9fa4-4024-8c0e-786949ce8af4.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -45,6 +45,9 @@ function Router() {
 
   return (
     <Switch>
+      <Route path="/">
+        <Redirect to="/dashboard" />
+      </Route>
       <Route path="/dashboard" component={() => <PrivateRoute component={Dashboard} />} />
       <Route path="/dashboard/projects" component={() => <PrivateRoute component={Projects} />} />
       <Route path="/dashboard/tasks" component={() => <PrivateRoute component={Tasks} />} />
